59079f3eb1489cd95c70bed5f219ffe612444c77
Zalonewolf <odoabuchivictor7@gmail.com>
2022-09-29 20:11:24 +0100

    mystore

---
+++ products/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html',
+                  {'products':products})
+
+
+def offers(request):
+    return HttpResponse('offers')
+
+
+def new(request):
+    return HttpResponse('New Products')
+
+
+
+
